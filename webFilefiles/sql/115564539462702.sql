c8f241686dadbd07a98b0810b6372bbfcd28551f
haito <haitongjie@qq.com>
2019-09-07 11:20:20 +0800

    delete sql file

--- webFilefiles/sql/115564539462702.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM permission.user;
-select now();
-set global time_zone = '+8:00';
-use permission;
-INSERT INTO user
- (id,username,password)
-VALUES (null,"test","123456");
-select count(*) from user
